b583c83f31a4f5edd29b461b395a860857fd3b85
Fernando Amorim <fernandooamorim-bot@users.noreply.github.com>
2026-08-03 15:22:30 -0300

    Ajusta selecao de fotos da galeria

--- js/config.js
+++ js/config.js
@@ -27,23 +27,17 @@ window.WEDDING_CONFIG = {
   mapsUrl: "https://www.google.com/maps/search/?api=1&query=Buffet%20La%20Maison%20Av.%20Eng.%20Luiz%20Vieira%20555%20Papicu",
   defaultGiftImage: "assets/images/gifts/presente.svg",
   gallery: [
-    "_BQH1948.jpg",
     "_BQH1929-Editar.jpg",
     "_BQH1920.jpg",
     "_BQH1926.jpg",
     "_BQH1940.jpg",
     "_BQH1942.jpg",
     "_BQH1948.jpg",
-    "_BQH1952.jpg",
     "_BQH1958.jpg",
     "_BQH1961.jpg",
     "_BQH1964.jpg",
     "_BQH1966.jpg",
-    "_BQH1966-2.jpg",
-    "IMG_1680.JPG",
-    "IMG_1684.JPG",
-    "IMG_1685.JPG",
-    "IMG_1686.JPG"
+    "_BQH1966-2.jpg"
   ],
   fallbackData: {
     guests: [
